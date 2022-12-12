325c048a95ab5d3cfb99835cd28fa96a11bde135
Татьяна Николаева <tanya8621@mail.ru>
2022-12-12 11:59:47 +0300

    3

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-
+from rest_framework.authtoken import views
 from .views import CommentViewSet, GroupViewSet, PostViewSet
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = DefaultRouter()
 
@@ -15,4 +16,13 @@ router.register('groups', GroupViewSet, basename='groups')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
+    path('v1/api-token-auth/', views.obtain_auth_token),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
